6dc1ae82a4f5001ffe5546a91520b50e7ad35d68
Juan M <juanm@juan.local>
2024-11-18 11:05:18 -0500

    PEER-8 Only show Join and Manage Groups options for upcoming Sessions.

--- src/components/sessions/SessionList.js
+++ src/components/sessions/SessionList.js
@@ -27,13 +27,17 @@ const SessionList = ({sessions, setSession}) => {
         return (
           <Fragment key={session.id}>
             <div className="flex center-vertical gap-xs">
-              <span className="text color-main size-md">{session.isCompleted ? 'Closed' : 'Upcoming'}</span>
+              <span className="text color-main size-md">{session.isCompleted ? 'Finished' : 'Upcoming'}</span>
               <span className="text bold size-lg"><SessionName session={session} /></span>
             </div>
             <div className="flex wrap gap-sm">
               <div className="padding-left-right-sm action-element" onClick={() => setSession(session)}>Edit</div>
-              <div className="padding-left-right-sm action-element" onClick={() => window.location.href = `/session/${session.id}`}>Join</div>
-              <div className="padding-left-right-sm action-element" onClick={() => window.location.href = `/session/${session.id}/ubq6nqwo4nd7a3infg`}>Manage Groups</div>
+              {!session.isCompleted && (
+                <>
+                  <div className="padding-left-right-sm action-element" onClick={() => window.location.href = `/session/${session.id}`}>Join</div>
+                  <div className="padding-left-right-sm action-element" onClick={() => window.location.href = `/session/${session.id}/ubq6nqwo4nd7a3infg`}>Manage Groups</div>
+                </>
+              )}
             </div>
             <div className="padding-top-bottom-sm">
               <div className="horizontal-line"></div>
